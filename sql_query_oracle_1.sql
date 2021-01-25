88e7b47a63d6fabe0689fe6fec78934e85eaafff
Yuriy <68117801+vol1ura@users.noreply.github.com>
2021-01-25 21:45:24 +0300

    add comments signs

--- sql_query_oracle_1.sql
+++ sql_query_oracle_1.sql
@@ -1,18 +1,19 @@
+/*
 [1] https://www.hackerrank.com/challenges/weather-observation-station-11/problem
 * Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. 
 * Your result cannot contain duplicates.
 
-My solution:
+My solution: */
 SELECT distinct CITY FROM STATION WHERE not LOWER(SUBSTR(CITY,1,1)) in ('a','e','i','o','u') or not LOWER(SUBSTR(CITY,-1,1)) in ('a','e','i','o','u');
-
+/*
 [2] https://www.hackerrank.com/challenges/more-than-75-marks/problem
 *Query the Name of any student in STUDENTS who scored higher than Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
 
-My solution:
+My solution: */
 SELECT Name FROM STUDENTS WHERE Marks > 75 ORDER BY SUBSTR(Name,-3,3), ID ASC;
-
+/*
 [3] https://www.hackerrank.com/challenges/salary-of-employees/problem
 * Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than per month who have been employees for less than months. Sort your result by ascending employee_id.
 
-My solution:
+My solution: */
 SELECT name FROM Employee WHERE salary > 2000 AND months < 10 ORDER BY employee_id;
