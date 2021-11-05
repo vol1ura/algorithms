c18dcc38335674c00031d3a524e2120db05f73a4
Yuriy Volodin <vjv@inbox.ru>
2021-11-05 17:46:20 +0300

    add Lucas numbers problem

---
+++ Ruby/Lucas_numbers.rb
@@ -0,0 +1,51 @@
+# https://www.codewars.com/kata/55a7de09273f6652b200002e/ruby
+# Lucas numbers are numbers in a sequence defined like this:
+
+# L(0) = 2
+# L(1) = 1
+# L(n) = L(n-1) + L(n-2)
+
+# Your mission is to complete the function that returns the nth term of this sequence.
+
+# Note: It should work for negative numbers as well; how you do this is you flip the equation around, so for negative numbers: L(n) = L(n+2) - L(n+1)
+
+
+# 1 solution - recurrent
+
+def lucasnum(n)
+  return 2 if n == 0
+  return 1 if n == 1
+  
+  l0, l1 = 2, 1
+  if n > 0
+    2.upto(n) { l0, l1 = l1, l0 + l1 }
+    l1
+  else
+    n.upto(-1) { l0, l1 = l1 - l0, l0 }
+    l0
+  end
+end
+
+# 2 solution - recursive with memoize
+
+@cache = Hash.new(nil)
+
+def lucasnum(n)
+  return 2 if n == 0
+  return 1 if n == 1
+  return @cache[n] if @cache.member?(n)
+
+  @cache[n] = n > 0 ? lucasnum(n-1) + lucasnum(n-2) : lucasnum(n+2) - lucasnum(n+1)
+  @cache[n]
+end
+
+# Examples
+
+# L(-10) = 123
+# L(-5)  = -11
+# L(-1)  =  -1
+# L(0)   =   2
+# L(1)   =   1
+# L(5)   =  11
+# L(10)  = 123
+
